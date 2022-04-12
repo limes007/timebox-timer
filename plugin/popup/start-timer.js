e22d784b2cb3a77dc2523db59a44f9452b653b12
limes007 <39467727+limes007@users.noreply.github.com>
2022-04-12 16:23:39 +0200

    remember last timebox value in storage #2

--- plugin/popup/start-timer.js
+++ plugin/popup/start-timer.js
@@ -1,12 +1,24 @@
+let tb_config = {
+	'timebox': 15,
+};
+
+browser.storage.local.get()
+.then((config) => {
+	tb_config = {...tb_config, ...config};
+	document.getElementById("timebox-input").value = tb_config.timebox;
+});
+
 document.addEventListener("click", (e) => {
 
 	function startTimerAndClosePopup(tabs) {
 		browser.tabs.executeScript({file: "/content_scripts/timebox-timer.js"})
 			.then(() => {
-				let tb = document.getElementById("timebox-input").value;
+				tb_config.timebox = document.getElementById("timebox-input").value;
+				browser.storage.local.set(tb_config);
+
 				browser.tabs.sendMessage(tabs[0].id, {
 					command: "start-timer",
-					timebox: tb
+					timebox: tb_config.timebox
 				})
 				.then(() => {
 					window.close();
